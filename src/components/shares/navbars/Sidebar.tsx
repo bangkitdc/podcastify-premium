b1b1fb7f1d733f945cebe10002610879c6c15510
irsyadnb <irsyadxd@gmail.com>
2023-10-22 21:20:03 +0700

    feat: sidebar and topbar

---
+++ src/components/shares/navbars/Sidebar.tsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+function Sidebar({
+  navList,
+  currentUrl,
+  // value,
+  // setValue,
+}: {
+  navList: Record<string, string[]>;
+  currentUrl: string
+  // value: string;
+  // setValue: (event: string) => void;
+}) {
+  return (
+    <div className="flex flex-col px-2 py-4 bg-clr-background-base-two rounded-lg gap-2 col-span-1 row-span-6 font-medium">
+      {Object.entries(navList).map(([url, [displayName, iconUrl]]) => (
+        <Link
+          to={url}
+          className={`flex justify-start gap-6 items-center opacity-70 cursor-pointer py-2 px-2 font-bold ${currentUrl === url ? 'opacity-100' : ''}`}
+          // onClick={() => setValue(url)}
+        >
+          <img src={iconUrl} className="overflow-hidden" />
+          <div>{displayName}</div>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/components/shares/navbars/Topbar.tsx
@@ -0,0 +1,9 @@
+function Topbar() {
+  return (
+    <div className=" sticky top-0 col-span-3 flex justify-between py-4 px-6 items-center z-[2]">
+      Profile Topbar
+    </div>
+  )
+}
+
+export default Topbar
