fd10c44a8852d30e675027dea8c2e9549fbe33ab
irsyadnb <irsyadxd@gmail.com>
2023-10-25 12:30:14 +0700

    chore: add button width

--- src/components/shares/buttons/AddButton.tsx
+++ src/components/shares/buttons/AddButton.tsx
@@ -2,7 +2,7 @@ export default function AddButton({ text }: { text: string }) {
   return (
     <button
       type="submit"
-      className="p-2 w-32 text-clr-text-black bg-clr-text-info outline-none border-none rounded-3xl px-5 text-sm hover:bg-clr-text-info-hover"
+      className="p-2 w-36 text-clr-text-black bg-clr-text-info outline-none border-none rounded-3xl px-5 text-sm hover:bg-clr-text-info-hover"
     >
       {text}
     </button>
