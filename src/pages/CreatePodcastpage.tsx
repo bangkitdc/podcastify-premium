751ba6ad90e3a0108774f6d85d7ca683883468a1
irsyadnb <irsyadxd@gmail.com>
2023-10-25 12:32:41 +0700

    chore: create form

--- src/pages/CreatePodcastpage.tsx
+++ src/pages/CreatePodcastpage.tsx
@@ -1,7 +1,7 @@
 import AddButton from "../components/shares/buttons/AddButton";
 import BaseInputText from "../components/shares/inputs/BaseInputText";
 import BaseSelect from "../components/shares/inputs/BaseSelect";
-import BaseUploader from "../components/shares/uploads/BaseUploader";
+import BaseUploader from "../components/shares/uploads/BaseFileUploader";
 import useFile from "../hooks/useFile";
 import useInput from "../hooks/useInput";
 
@@ -10,7 +10,7 @@ function CreatePodcastPage() {
   const [description, setDescription] = useInput("");
   const [imageFile, setImageFile] = useFile(null);
 
-  const categories = ["Comedy", "Horror", "Deep Talk", "18+"]
+  const categories = ["Comedy", "Horror", "Deep Talk", "18+"];
   return (
     <form className="flex flex-col gap-3">
       <BaseInputText
@@ -29,11 +29,11 @@ function CreatePodcastPage() {
         value={description}
         setValue={setDescription}
       />
-      <BaseSelect options={categories} label="Category" id="testselect"/>
+      <BaseSelect options={categories} label="Category" id="testselect" />
       <BaseUploader
         id="episode-poster-upload"
         type="image"
-        label="Upload a poster file :"
+        label="Poster File :"
         value={imageFile}
         setValue={setImageFile}
       />
