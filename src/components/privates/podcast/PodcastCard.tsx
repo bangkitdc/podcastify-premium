18ffad91562e449f778a5ab33e590cfe174cb58b
irsyadnb <irsyadxd@gmail.com>
2023-10-28 01:29:51 +0700

    feat: delete on podcast card

--- src/components/privates/podcast/PodcastCard.tsx
+++ src/components/privates/podcast/PodcastCard.tsx
@@ -1,5 +1,5 @@
 import { ICONS_DIR, IMAGES_DIR } from "../../../config/config";
-function PodcastCard({podcastData, img_url, handleManageModal }: {podcastData: string[]; img_url: string, handleManageModal : (id?: string) => void }) {
+function PodcastCard({podcastData, img_url, handleEditModal, handleDeleteModal }: {podcastData: string[]; img_url: string, handleEditModal : (id?: string) => void, handleDeleteModal : (id?: string) => void }) {
   
   return (
     <div className=" bg-clr-background-card p-4 rounded-lg h-fit shadow cursor-pointer flex flex-col hover:bg-clr-background-highlight-two">
@@ -17,8 +17,9 @@ function PodcastCard({podcastData, img_url, handleManageModal }: {podcastData: s
       <p className=" overflow-hidden whitespace-nowrap text-ellipsis">
         {podcastData[0]}
       </p>
-      <div>
-        <img onClick={() => handleManageModal(podcastData[2])} src={ICONS_DIR + "edit.svg"} alt="" />
+      <div className="flex justify-between">
+        <img onClick={() => handleEditModal(podcastData[2])} src={ICONS_DIR + "edit.svg"} alt="" />
+        <img onClick={() => handleDeleteModal(podcastData[2])} src={ICONS_DIR + "trash.svg"} alt="" />
       </div>
     </div>
   );
