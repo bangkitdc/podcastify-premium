e21df348f685ba4c8330b5c90634c68b181f67a6
irsyadnb <irsyadxd@gmail.com>
2023-11-07 00:04:04 +0700

    chore(episode): renew table and detail page

--- src/components/shares/tables/TablesData.tsx
+++ src/components/shares/tables/TablesData.tsx
@@ -1,48 +1,51 @@
 import { ICONS_DIR } from "@/config/config";
 
 function TablesData({
-  data,
+  dataContext,
+  dataContent,
   onClickManage,
   onNavigate,
 }: {
-  data: string[][];
-  onClickManage?: (id?: string) => void;
-  onNavigate?: (id: string) => void;
+  dataContext: string[];
+  dataContent: (string | number)[];
+  onClickManage?: () => void;
+  onNavigate?: () => void;
 }) {
+  const handleClickManage = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    if (onClickManage) {
+      onClickManage();
+    }
+  };
   return (
     <>
-      <tr>
-        <td colSpan={5}></td>
-      </tr>
-      {data.map((d, index) => (
-        <tr
-          className={` group hover:bg-clr-background-highlight-two ${
-            onNavigate ? " cursor-pointer" : ""
-          }`}
-          onClick={onNavigate ? () => onNavigate(d[0]) : undefined}
-        >
-          <td>{index + 1}</td>
-          {d.slice(0, d.length - 1).map((_, index) => {
-            return (
-              <td>
-                <p className=" group-hover:text-clr-text-primary">{d[index + 1]}</p>
+      <tr
+        className={` group hover:bg-clr-background-highlight-two ${
+          onNavigate ? " cursor-pointer" : ""
+        }`}
+        onClick={onNavigate ? () => onNavigate() : undefined}
+      >
+        {dataContent.map((d, index) => {
+          return (
+            <>
+              <td key={dataContext[index]}>
+                <p className=" group-hover:text-clr-text-primary font-thin">
+                  {d}
+                </p>
               </td>
-            );
-          })}
-          {onClickManage ? (
-            <td className="flex gap-2 relative">
-              <img
-                className=" h-5 w-5 hidden group-hover:block cursor-pointer"
-                src={ICONS_DIR + "three-dots.svg"}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  onClickManage(d[0]);
-                }}
-              />
-            </td>
-          ) : null}
-        </tr>
-      ))}
+            </>
+          );
+        })}
+        {onClickManage ? (
+          <td className="flex gap-2 relative">
+            <img
+              className=" h-5 w-5 hidden group-hover:block cursor-pointer"
+              src={ICONS_DIR + "three-dots.svg"}
+              onClick={handleClickManage}
+            />
+          </td>
+        ) : null}
+      </tr>
     </>
   );
 }
--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,10 +1,16 @@
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent } from "react";
 
-function useInput(defaultValue: string = ''): [string, (e: ChangeEvent<HTMLInputElement>) => void] {
+function useInput(
+  defaultValue: string = ""
+): [string, (e: ChangeEvent<HTMLInputElement> | string) => void] {
   const [value, setValue] = useState(defaultValue);
 
-  function handleValueChange(event: ChangeEvent<HTMLInputElement>) {
-    setValue(event.target.value);
+  function handleValueChange(event: ChangeEvent<HTMLInputElement> | string) {
+    if (typeof event === "string") {
+      setValue(event);
+    } else {
+      setValue(event.target.value);
+    }
   }
 
   return [value, handleValueChange];
