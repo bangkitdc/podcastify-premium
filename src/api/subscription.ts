ff29a21fac82886c115cb83dc244397730b9a382
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-11-08 00:25:26 +0700

    chore: add subscription api hit

---
+++ src/api/subscription.ts
@@ -0,0 +1,38 @@
+import { IApiBaseResponse } from '@/types/http';
+import support from './support';
+import {
+  IApiBaseSubscription,
+  ISubscriptionRequest,
+} from '@/types/subscription';
+
+const subscription = () => {
+  const { api, apiUrl } = support();
+
+  const url = {
+    subscription: apiUrl.subscription,
+  };
+
+  const getAllSubscriptions = async () => {
+    const response = await api.get<IApiBaseResponse<IApiBaseSubscription[]>>(
+      url.subscription,
+    );
+
+    return response.data;
+  };
+
+  const updateSubscriptionStatus = async (payload: ISubscriptionRequest) => {
+    const response = await api.patch<IApiBaseResponse<IApiBaseSubscription>>(
+      url.subscription,
+      payload,
+    );
+
+    return response.data;
+  };
+
+  return {
+    getAllSubscriptions,
+    updateSubscriptionStatus,
+  };
+};
+
+export default subscription;
--- src/api/support.ts
+++ src/api/support.ts
@@ -1,24 +1,26 @@
-import axios from "axios";
+import axios from 'axios';
 
 const url = import.meta.env.VITE_API_BASE_URL;
 const api = axios.create({
   baseURL: url,
-  withCredentials: true
+  withCredentials: true,
 });
 
-const support = () => {  
+const support = () => {
   const apiUrl = {
     login: '/login',
     register: '/register',
     refreshToken: '/refresh_token',
     logout: '/logout',
 
-    episode: "/episode",
-    
+    episode: '/episode',
+
+    subscription: '/subscription',
+
     self: '/self',
-  }
+  };
 
   return { api, apiUrl };
-}
+};
 
 export default support;
