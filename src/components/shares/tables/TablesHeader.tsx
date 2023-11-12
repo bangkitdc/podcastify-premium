601b62f3f50d4bc812bad3656cb08e154eab0e37
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-11-13 00:35:24 +0700

    feat(tables): add colsClass to style table cols

--- src/components/shares/tables/TablesHeader.tsx
+++ src/components/shares/tables/TablesHeader.tsx
@@ -1,12 +1,29 @@
-function TablesHeader({headers, percentage} : {headers: string[], percentage: number[]}) {
+function TablesHeader({
+  headers,
+  percentage,
+  colsClass,
+}: {
+  headers: string[];
+  percentage: number[];
+  colsClass?: string[];
+}) {
   return (
     <tr>
-      <th className="w-[5%]" style={{width: "5%"}}>#</th>
+      <th className="w-[5%]" style={{ width: '5%' }}>
+        #
+      </th>
       {headers.map((h, index) => (
-        <th style={{width: percentage[index] + "%"}} key={index}>{h}</th>
+        <th
+          className={`${colsClass ? colsClass[index] : ''} w-[${
+            percentage[index]
+          }%]`}
+          key={index}
+        >
+          {h}
+        </th>
       ))}
     </tr>
   );
 }
 
-export default TablesHeader
+export default TablesHeader;
